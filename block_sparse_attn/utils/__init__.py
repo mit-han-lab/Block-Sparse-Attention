6ee79e3e1ba6f0346037141c9178abca0569ef1c
LujiaJin <jinlujia@pku.edu.cn>
2026-01-09 13:10:37 +0800

    Fix import issues in FlashVSR integration
    - Comment out flash_attn_interface imports (not present in block_sparse_attn)
    - Fix circular import issue in utils/__init__.py
    - Ensure clean imports for FlashVSR compatibility

--- block_sparse_attn/utils/__init__.py
+++ block_sparse_attn/utils/__init__.py
@@ -1,10 +1,1 @@
-# block_sparse_attn/__init__.py
-__version__ = "0.0.1"
-
-from block_sparse_attn.block_sparse_attn_interface import (
-    block_sparse_attn_func,
-    block_streaming_attn_func,
-    token_streaming_attn_func
-)
-
-from . import utils
+# block_sparse_attn.utils package
